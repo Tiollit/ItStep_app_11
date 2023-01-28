d5b6a74f8ad88347df12c422efff52a676b82911
Tiollit <Tiollit7@gmail.com>
2023-01-28 06:17:54 -0800

    g

--- Student.h
+++ Student.h
@@ -1,1 +1,18 @@
-#pragma once
+#include <iostream>
+using namespace std;
+class Student {
+private:
+    string Student;
+public:
+    int date;
+    string PIB, phone, town, country, study, groupe;
+    Student();
+    Test(int age);
+    Test(string text, int age);
+       
+    void set_text(string text);
+     
+    string get_text();
+     
+    ~Test();
+};
